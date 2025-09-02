9eb5503acfe84d6254ccea2263bdb3bfe3e4053f
Tejas Shingala <shingalatejas254@gmail.com>
2025-09-02 00:40:04 -1200

    added typescript

---
+++ build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let username = "John Doe";
+console.log(`Hello, ${username}!`);
+let a = 10;
+let b = 20;
+let c = 15;
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+function isPrime(num: number): boolean {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+const a: number = 17; // Change the number here
+if (isPrime(a)) {
+  console.log(`${a} is a Prime Number`);
+} else {
+  console.log(`${a} is NOT a Prime Number`);
+}
